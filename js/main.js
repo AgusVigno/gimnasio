afe1f2feb299ea82a81e758df4ff98094743b4f6
Agustin Vignolo <agus88lp@gmail.com>
2020-06-05 13:43:43 -0300

    Animacion de numeros y contador

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function(){
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', function(){
+        //Si pagina actual == home carga el MAPA
+        if(document.getElementById('mapa')){
+            var map = L.map('mapa').setView([-34.919851, -57.95404], 16);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([-34.919851, -57.95404]).addTo(map)
+                .bindPopup('GymLaPlata')
+                .openPopup();
+        }
+
+        // Animaciones Numeros
+        $('.resumenInfo li:nth-child(1) p').animateNumber({ number: 6 }, 1200);
+        $('.resumenInfo li:nth-child(2) p').animateNumber({ number: 15 }, 1200);
+        $('.resumenInfo li:nth-child(3) p').animateNumber({ number: 3 }, 1500);
+        $('.resumenInfo li:nth-child(4) p').animateNumber({ number: 9 }, 1500);
+
+        // Cuenta Regresiva
+        $('.cuentaRegresiva').countdown('2020/07/20 10:00:00', function(event){
+            $('#dias').html(event.strftime('%D'));
+            $('#horas').html(event.strftime('%H'));
+            $('#minutos').html(event.strftime('%M'));
+            $('#segundos').html(event.strftime('%S'));
+        });
+
+    });
+})();
+
